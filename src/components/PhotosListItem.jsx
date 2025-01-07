272c2b557771869deaebc53166ed822bede3ad97
mohammadzakijafari <zaki.jafari5@gmail.com>
2025-01-07 20:18:03 +0100

    media, image url generation correction

--- src/components/PhotosListItem.jsx
+++ src/components/PhotosListItem.jsx
@@ -11,9 +11,11 @@ const PhotosListItem = ({ photo }) => {
     const handleRemovePhoto = () => {
         removePhoto(photo);
     }
+    {console.log(photo.url)}
   return (
     <div onClick={handleRemovePhoto} className='relative cursor-pointer m-2'>
-        <img src = {photo.url} className='h-36 w-36' />
+      
+        <img src={photo.url} className='h-36 w-36' alt='random pic' />
         <div className='absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80'>
             <MdDeleteOutline className = 'text-4xl' />
         </div>
--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -1,8 +1,19 @@
 import { faker } from "@faker-js/faker";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+
+function generateRandomImageUrl(width, height) {
+    return `https://picsum.photos/${width}/${height}`;
+}
+
+function firstRandom () {
+    return Math.floor(Math.random() * 100);
+}
+function secondRandom () {
+    return Math.floor(Math.random() * 200);
+}
 const photosApi = createApi({
-    reducerPath: 'Photos',
+    reducerPath: 'photos',
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3002'}),
     endpoints: (builder) => ({
         removePhoto: builder.mutation({
@@ -13,16 +24,15 @@ const photosApi = createApi({
             })
         }),
         addPhoto: builder.mutation({
-            invalidatesTags: (result, error, photo) => [{type: 'AlbumPhoto', id: album.id}],
-            query: (photo) => ({
+            invalidatesTags: (result, error, album) => [{type: 'AlbumPhoto', id: album.id}],
+            query: (album) => ({
                 url: '/photos',
                 method: 'POST',
-                body: {PhotoId: photo.id, url: faker.image.abstract(150, 150, true)},
+                body: {albumId: album.id, url: generateRandomImageUrl(firstRandom(), secondRandom())},
             })
         }),
         fetchPhotos: builder.query({
             providesTags: (result, error, album) => {
-                if (!result) return [{ type: 'AlbumPhoto', id: album.id }];
                 const tags = result.map((photo) => ({ type: 'Photo', id: photo.id }));
                 tags.push({ type: 'AlbumPhoto', id: album.id });
                 return tags;
